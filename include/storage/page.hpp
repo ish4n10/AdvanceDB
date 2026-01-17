c59849100bdb09200f75e95e5d310ddffd3738ee
ish4n10 <ishan.tripathi010@gmail.com>
2026-01-17 23:24:29 +0530

    add disk manager

---
+++ include/storage/page.hpp
@@ -0,0 +1,34 @@
+#include <cstdint>
+enum class PageType : uint16_t {
+    HEADER = 0,
+    META = 1,
+    INDEX = 2,
+    DATA = 3,
+    FREE = 4
+};
+
+enum class PageLevel : uint16_t {
+    LEAF = 0,
+    INTERNAL = 1
+};
+
+// ensuring single byte of alignment for each field in the struct
+#pragma pack(push, 1)
+struct PageHeader {
+    uint32_t page_id;
+    PageType page_type;
+    PageLevel page_level;
+
+    // might need to take a look at struct again
+    uint8_t reserved[8];
+    uint16_t flags;
+    uint16_t cell_count; 
+    uint16_t free_start;
+    uint16_t free_end;
+
+    uint32_t parent_page_id;
+    uint32_t lsn; 
+};
+#pragma pack(pop)
+
+static_assert(sizeof(PageHeader) ==  32, "PageHeader size must be 32 bytes");
---
+++ src/storage/disk_manager.cpp
@@ -0,0 +1,59 @@
+#include "storage/disk_manager.hpp"
+#include "common/constants.hpp"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdexcept>
+#include <iostream>
+#include <cstring>
+
+DiskManager::DiskManager(std::string& file_path) {
+    // 0644 is the permission setting for the created file
+    // each number represents user, group, others respectively
+    file_descriptor = open(file_path.c_str(), O_RDWR | O_CREAT, 0644);
+
+    if (file_descriptor < 0) {
+        throw std::runtime_error("Failed to open or create file");
+    }
+}
+
+DiskManager::~DiskManager() {
+    if (file_descriptor >= 0) {
+        close(file_descriptor);
+    }
+}
+
+void DiskManager::read_page(int page_id, char* page_data) {
+    long offset = static_cast<long>(page_id * PAGE_SIZE);
+    if (lseek(file_descriptor, offset, SEEK_SET) < 0) {
+        throw std::runtime_error("Failed to seek to the correct position for reading");
+    }
+    ssize_t bytes_read = read(file_descriptor, page_data, PAGE_SIZE); // read after lseek
+    std::cout << "Read " << bytes_read << " bytes from page " << page_id << std::endl;
+
+    if (bytes_read < PAGE_SIZE) {
+        std::fill_n(page_data + bytes_read, PAGE_SIZE - bytes_read, 0); // zero the rest
+    }
+}
+
+void DiskManager::write_page(int page_id, const void* page_data) {
+    long offset = static_cast<long>(page_id * PAGE_SIZE); 
+
+    if (lseek(file_descriptor, offset, SEEK_SET) < 0) {
+        throw std::runtime_error("Failed to seek to the correct position for writing");
+    }
+    ssize_t bytes_written = write(file_descriptor, page_data, PAGE_SIZE); 
+
+    std::cout << "Wrote " << bytes_written << " bytes to page " << page_id << std::endl;
+
+    if (bytes_written != PAGE_SIZE) {
+        throw std::runtime_error("Failed to write the complete page");
+    }
+}
+
+void DiskManager::flush() {
+
+    // TODO: Use fsync on Unixbased systems
+    if (_commit(file_descriptor) < 0) {
+        throw std::runtime_error("Failed to flush data to disk");
+    }
+}
